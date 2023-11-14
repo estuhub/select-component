0235e06357353a3f0a7c864cd985e5e8892e8796
estuhub <107235863+estuhub@users.noreply.github.com>
2023-11-14 11:56:51 +0700

    functional single select option

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react"
-import { Select } from "./Select"
+import { Select, SelectOption } from "./Select"
+
+const options = [
+  { label: 'chocolate', value: 1 },
+  { label: 'vanilla', value: 2 },
+  { label: 'strawberry', value: 3 },
+  { label: 'coffee', value: 4 },
+  { label: 'mint', value: 5 },
+]
 
 function App() {
-  const options = [
-    { label: 'chocolate', value: 1 },
-    { label: 'vanilla', value: 2 },
-    { label: 'strawberry', value: 3 },
-    { label: 'coffee', value: 4 },
-    { label: 'mint', value: 5 },
-  ]
-
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0])
+  const [value, setValue] = useState<SelectOption | undefined>()
 
   return <Select options={options} value={value} onChange={o => setValue(o)}/>
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react"
 import styles from "./select.module.css"
 
-type SelectOption = {
+export type SelectOption = {
     label: string,
-    value: any,
+    value: string | number,
 }
 
 type SelectProps = {
@@ -12,15 +13,62 @@ type SelectProps = {
 }
 
 export function Select({ value, onChange, options }: SelectProps) {
+    const [isOpen, setIsOpen] = useState(false)
+    const [highlightedIndex, setHighlightedIndex] = useState(undefined)
+
+    function clearOptions() {
+        onChange(undefined)
+    }
+
+    function selectOption(option: SelectOption) {
+        if (option !== value) onChange(option)
+    }
+
+    function isOptionSelected(option: SelectOption) {
+        return option === value
+    }
+
+    useEffect(() => {
+        if (isOpen) setHighlightedIndex(undefined)
+    }, [isOpen])
+
     return (
-        <div tabIndex={0} className={styles.container}>
+        <div 
+            onBlur={() => setIsOpen(false)} // closes the options when clicking somewhere else
+            onClick={() => setIsOpen(prev => !prev)} // opens or closes when clicking on the div
+            tabIndex={0} // specify the element is focusable and to define the order in which it receive focus using the tab
+            className={styles.container}
+        >
             <span className={styles.value}>{value?.label}</span>
-            <button className={styles["clear-btn"]}>&times;</button>
+            <button 
+                onClick={e => {
+                    e.stopPropagation() // stops the click event from going to the parent div
+                    clearOptions()
+                }}
+                className={styles["clear-btn"]}
+            >
+                &times;
+            </button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
-            <ul className={`${styles.options} ${styles.show}`}>
-                {options.map(option => (
-                    <li key={option.label} className={styles.option}>{option.label}</li>
+            <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+                {options.map((option, index) => (
+                    <li 
+                        onClick={e => {
+                            e.stopPropagation()
+                            selectOption(option)
+                            setIsOpen(false)
+                        }}
+                        onMouseEnter={() => setHighlightedIndex(index)}
+                        key={option.value} 
+                        className={`
+                            ${styles.option}
+                            ${isOptionSelected(option) ? styles.selected : ""}
+                            ${index === highlightedIndex ? styles.highlighted : ""}
+                        `}
+                    >
+                        {option.label}
+                    </li>
                 ))}
             </ul>
         </div>
