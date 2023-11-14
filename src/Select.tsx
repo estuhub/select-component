2972375278f152cb78e8d07a560d21cf9c0d9082
estuhub <107235863+estuhub@users.noreply.github.com>
2023-11-14 13:16:27 +0700

    add keyboard functionality

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from "./select.module.css"
 
 export type SelectOption = {
@@ -24,7 +24,8 @@ type SelectProps = {
 
 export function Select({ multiple, value, onChange, options }: SelectProps) {
     const [isOpen, setIsOpen] = useState(false)
-    const [highlightedIndex, setHighlightedIndex] = useState<number | undefined>(undefined)
+    const [highlightedIndex, setHighlightedIndex] = useState(0)
+    const containerRef = useRef<HTMLDivElement>(null)
 
     function clearOptions() {
         multiple ? onChange([]) : onChange(undefined)
@@ -49,11 +50,58 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
     }
 
     useEffect(() => {
-        if (isOpen) setHighlightedIndex(undefined)
+        if (isOpen) setHighlightedIndex(0)
     }, [isOpen])
+
+    // add functionality using keyboard commands
+    useEffect(() => {
+        // define the keyboard event handler
+        const handler = (e: KeyboardEvent) => {
+            // check if the event target is not the container
+            if (e.target !== containerRef.current) return
+            // handle different keyboard events
+            switch (e.code) {
+                // open or close the options on "Enter" or "Space" key press
+                case "Enter":
+                case "Space":
+                    setIsOpen(prev => !prev)
+                    // if the dropdown is open, select the option highlighted
+                    if (isOpen) selectOption(options[highlightedIndex])
+                    break
+                // navigate through options using arrow keys
+                case "ArrowUp":
+                case "ArrowDown": {
+                    // open the dropdown if it's closed
+                    if (!isOpen) {
+                        setIsOpen(true)
+                        break
+                    }
+                    // calculate the new highlighted index based on key press
+                    const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+                    // update the highlighted index within valid bounds
+                    if (newValue >= 0 && newValue < options.length) {
+                        setHighlightedIndex(newValue)
+                    }
+                    break
+                } // add curly braces because of declaring variables inside a switch-case
+                // close the dropdown on "Escape" key press
+                case "Escape":
+                    setIsOpen(false)
+                    break
+            }
+        }
+
+        // add the event listener to the container
+        containerRef.current?.addEventListener("keydown", handler)
+        // clean up by removing the event listener when the component unmounts
+        return () => {
+            containerRef.current?.removeEventListener("keydown", handler)
+        }
+    }, [isOpen, highlightedIndex, options])
 
     return (
         <div 
+            ref={containerRef}
             onBlur={() => setIsOpen(false)} // closes the options when clicking somewhere else
             onClick={() => setIsOpen(prev => !prev)} // opens or closes when clicking on the div
             tabIndex={0} // specify the element is focusable and to define the order in which it receive focus using the tab
